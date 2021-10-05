e0c265bc8d6c833c9b3506c624c7813226c28e1f
ioanaionescu <56482373+ioanaionescu@users.noreply.github.com>
2021-10-05 22:13:05 +0300

    ...

--- contact/js/script.js
+++ contact/js/script.js
@@ -240,7 +240,6 @@
     let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
     joint.rotation.y = THREE.Math.degToRad(degrees.x);
     joint.rotation.x = THREE.Math.degToRad(degrees.y);
-    console.log(joint.rotation.x);
   }
 
   function getMouseDegrees(x, y, degreeLimit) {
