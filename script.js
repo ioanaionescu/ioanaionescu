0da9f50ea9a42ce2fb8f4d6e07f1c68899127e75
ioanaionescu <56482373+ioanaionescu@users.noreply.github.com>
2021-10-05 20:53:27 +0300

    -

---
+++ script.js
@@ -0,0 +1,62 @@
+  var cards = document.querySelectorAll('.card');
+cards.forEach(card => card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+}));
+
+ var cards2 = document.querySelectorAll('.card2');
+cards2.forEach(card2 => card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+}));
+
+ var cards3 = document.querySelectorAll('.card3');
+cards3.forEach(card3 => card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+}));
+
+ var cards4 = document.querySelectorAll('.card4');
+cards4.forEach(card4 => card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+}));
+
+ var cards5 = document.querySelectorAll('.card5');
+cards5.forEach(card5 => card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+}));
+
+ var cards6 = document.querySelectorAll('.card6');
+cards6.forEach(card6 => card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+}));
+
+var cards7 = document.querySelectorAll('.card7');
+cards7.forEach(card7 => card7.addEventListener( 'click', function() {
+  card7.classList.toggle('is-flipped');
+}));
+
+var cards8 = document.querySelectorAll('.card8');
+cards8.forEach(card8 => card8.addEventListener( 'click', function() {
+  card8.classList.toggle('is-flipped');
+}));
+
+var cards9 = document.querySelectorAll('.card9');
+cards9.forEach(card9 => card9.addEventListener( 'click', function() {
+  card9.classList.toggle('is-flipped');
+}));
+
+var cards10 = document.querySelectorAll('.card10');
+cards10.forEach(card10 => card10.addEventListener( 'click', function() {
+  card10.classList.toggle('is-flipped');
+}));
+
+var cards11 = document.querySelectorAll('.card11');
+cards11.forEach(card11 => card11.addEventListener( 'click', function() {
+  card11.classList.toggle('is-flipped');
+}));
+
+var cards12 = document.querySelectorAll('.card12');
+cards12.forEach(card12 => card12.addEventListener( 'click', function() {
+  card12.classList.toggle('is-flipped');
+}));
+
+
+
